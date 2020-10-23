e9ac1c1f37aeba8de9646fc48a0f1ce8de7e03c0
Aionoso <wilisch-wwu@outlook.de>
2020-10-23 18:52:42 +0200

    added tor support

--- build/lib/krakipy/version.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.1.4"
-__author__ = "Hubertus Wilisch"
-__copyright__ = "Copyright 2020 Hubertus Wilisch"
-__license__ = "GNU GPLv3+"
-__url__ = "https://krakipy.readthedocs.io/en/latest/"
