781a579471029b5edb33fe3116ad59b4769563ea
Aionoso <wilisch-wwu@outlook.de>
2021-07-31 14:12:53 +0200

    Update version.py

--- src/krakipy/version.py
+++ src/krakipy/version.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "Hubertus Wilisch"
-__copyright__ = "Copyright 2020 Hubertus Wilisch"
+__copyright__ = "Copyright 2020-2021 Hubertus Wilisch"
 __license__ = "GNU GPLv3+"
 __url__ = "https://krakipy.readthedocs.io/en/latest/"
