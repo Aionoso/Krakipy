bc03eb116ecf0e66ed85cfff363a2be8353c7e25
Aionoso <Aionoso-software@outlook.de>
2025-10-14 17:07:44 +0200

    Update version.py - 1.11

--- src/krakipy/version.py
+++ src/krakipy/version.py
@@ -1,5 +1,6 @@
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 __author__ = "Hubertus Wilisch"
 __copyright__ = "Copyright 2020-2025 Hubertus Wilisch"
 __license__ = "MIT License"
+
 __url__ = "https://krakipy.readthedocs.io/en/latest/"
